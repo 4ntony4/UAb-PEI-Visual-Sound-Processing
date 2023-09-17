9b7862c8a414383c3f2e31f11fc97eba1b99893c
4ntony4 <63813933+4ntony4@users.noreply.github.com>
2023-09-17 21:32:39 +0000

    updated app.py

--- app.py
+++ app.py
@@ -1,12 +1,14 @@
-from flask import Flask, render_template
+import io
+import random
+
 import librosa
+from flask import Flask, Response, render_template
+from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
+from matplotlib.figure import Figure
+
 # import IPython.display as ipd
 
-app = Flask(
-	__name__,
-	template_folder='templates',
-	static_folder='static'
-)
+app = Flask(__name__, template_folder='templates', static_folder='static')
 
 @app.route("/")
 def home():
@@ -32,11 +34,6 @@ def example():
     printInfo(yyy())
     return 'hello'
 
-import io
-import random
-from flask import Response
-from matplotlib.backends.backend_agg import FigureCanvasAgg as FigureCanvas
-from matplotlib.figure import Figure
 
 @app.route('/plot.png')
 def plot_png():
